da26777afafe35559d5c1a0210f72a728d3b9dd3
Marti Belegu <48526054+mgbelegu@users.noreply.github.com>
2020-01-22 15:58:28 +0100

    Update

--- main.js
+++ main.js
@@ -33,3 +33,48 @@ $("#img3").on({
         this.src = 'images/img3.jpg';
     }
 });
+
+$("#logo1").on({
+    "mouseover": function () {
+        this.src = 'images/fb-logo-hover.png';
+    },
+    "mouseout": function () {
+        this.src = 'images/fb-logo.png';
+    }
+});
+
+$("#logo2").on({
+    "mouseover": function () {
+        this.src = 'images/twitter-logo-hover.png';
+    },
+    "mouseout": function () {
+        this.src = 'images/twitter-logo.png';
+    }
+});
+
+$("#logo3").on({
+    "mouseover": function () {
+        this.src = 'images/yt-logo-hover.png';
+    },
+    "mouseout": function () {
+        this.src = 'images/yt-logo.png';
+    }
+});
+
+$("#logo4").on({
+    "mouseover": function () {
+        this.src = 'images/pinterest-logo-hover.png';
+    },
+    "mouseout": function () {
+        this.src = 'images/pinterest-logo.png';
+    }
+});
+
+$("#logo5").on({
+    "mouseover": function () {
+        this.src = 'images/gplus-logo-hover.png';
+    },
+    "mouseout": function () {
+        this.src = 'images/gplus-logo.png';
+    }
+});
